28700f1bf8a7be2d58b125098a8b6df6d0ec94ee
nbonn <nbonnalgueal@gmail.com>
2024-03-01 13:20:09 +0100

    First Commit

---
+++ functions.py
@@ -0,0 +1,48 @@
+def greet(name):
+    """
+    Simple greet function, prints hello
+    :param name: string
+    :return: None
+    """
+    print(f"Hello, {name}")
+
+# greet("Patricio")
+# greet("Guillermo")
+# greet("Ximena")
+
+def special_op(x=10, y=10, z=10):
+    """
+    Some special operation
+    :param x: int or float
+    :param y: int or float
+    :param z: int or float
+    :return: the result of the operation
+    """
+    return x * y + z
+
+
+result = special_op(2, 3, 4)
+print(result)
+print(special_op(2, 3))
+print(special_op())
+print(special_op(z=2, x=3, y=4))
+
+
+
+greet(special_op(2, 3, 4))
+
+# recursive function:
+
+
+def factorial(n):
+    """
+    Factorial of a number
+    :param n: int number
+    :return: the value of n!
+    """
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+
+print(factorial(5)) # should be 120
